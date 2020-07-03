f71af7fda6404707d082d74150fb47c482127950
AlfredMorgan <amorgan@sidearmsports.com>
2020-07-03 17:37:26 -0400

    Revert "first commit, server side services are working few more to add."
    This reverts commit 08035020e8b20481fdeffd74c94e887b4e88a5f6.

--- Pokedex/client-app/src/repositorys/pokedexrepository.js
+++
@@ -1,9 +0,0 @@
-import Repository from "./Repository";
-
-const resource = "/pokedex";
-export default {
-    get_pokedex(){
-        console.log(resource)
-        return Repository.get(`${resource}/1/`);
-    },
-}
